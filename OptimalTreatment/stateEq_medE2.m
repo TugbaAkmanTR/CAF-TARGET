e704cb24ab903be738ba023da23a87484b233892
Tuğba Akman <94012098+TugbaAkmanTR@users.noreply.github.com>
2026-01-27 14:23:03 +0300

    Update stateEq_medE2.m

--- OptimalTreatment/stateEq_medE2.m
+++ OptimalTreatment/stateEq_medE2.m
@@ -1,10 +1,10 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % State equation 
 %
-% Author: Tuğba Akman Date: Jan 2023
+% Author: Tuğba Akman Date: Jan 2026
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-function dy = model_for_model_wo_CAFs_high_med_low(t,y,u1,u2,u3,Tu,E2)
+function dy = stateEq_medE2(t,y,u1,u2,u3,Tu,E2)
 
 global Eq
 
@@ -18,29 +18,13 @@ T1 = y(1);
 ER1 = y(2);
 E2ER1 = y(3);
 CAF21 = y(4);
-% T2 = y(5);
-% ER2 = y(6);
-% E2ER2 = y(7);
-% CAF22 = y(8);
-% T3 = y(9);
-% ER3 = y(10);
-% E2ER3 = y(11);
-% CAF23 = y(12);
 
 if t<=60
   RHS2 = Eq.beta*(1/(1+Eq.d3*CAF21)) - 0.0042*ER1 - (1-u3)*Eq.db*0.1*ER1 + 0.0417*E2ER1; %high
   RHS3 = -0.0125*E2ER1 + (1-u3)*Eq.db*0.1*ER1 - 0.0417*E2ER1;
-  % RHS6 = Eq.beta*(1/(1+Eq.d3*CAF22)) - 0.0042*ER2 - Eq.db*0.1*ER2 + 0.0417*E2ER2;%med
-  % RHS7 = -0.0125*E2ER2 + Eq.db*0.1*ER2 - 0.0417*E2ER2;
-  % RHS10 = Eq.beta*(1/(1+Eq.d3*CAF23)) - 0.0042*ER3 - Eq.db*0.025*ER3 + 0.0417*E2ER3;%low
-  % RHS11 = -0.0125*E2ER3 + Eq.db*0.025*ER3 - 0.0417*E2ER3;
 else
   RHS2 = Eq.beta*(1/(1+Eq.d3*CAF21)) - 0.0042*ER1 - Eq.db*0*ER1 + 0.0417*E2ER1;
   RHS3 = -0.0125*E2ER1 + Eq.db*0*ER1 - 0.0417*E2ER1;
-  % RHS6 = Eq.beta*(1/(1+Eq.d3*CAF22)) - 0.0042*ER2 - Eq.db*0*ER2 + 0.0417*E2ER2;
-  % RHS7 = -0.0125*E2ER2 + Eq.db*0*ER2 - 0.0417*E2ER2;
-  % RHS10 = Eq.beta*(1/(1+Eq.d3*CAF23)) - 0.0042*ER3 - Eq.db*0*ER3 + 0.0417*E2ER3;
-  % RHS11 = -0.0125*E2ER3 + Eq.db*0*ER3 - 0.0417*E2ER3;
 end
 
 dy(1) = T1*(1-(0.001*T1))*(Eq.k1_hat*(E2ER1/(Eq.alpha1 + E2ER1)) + (1-u2)*Eq.k2*(CAF21/(Eq.alpha2 + CAF21)));
@@ -48,14 +32,6 @@ dy(2) = RHS2;
 dy(3) = RHS3;
 dy(4) = (1-u1)*(Eq.k1_hat/3)*CAF21*(1-(0.001*CAF21))*(T1/(Eq.alpha3 + T1));
 
-% dy(6) = T2*(1-(0.001*T2))*(Eq.k1_hat*(E2ER2/(Eq.alpha1 + E2ER2)) + Eq.k2*(CAF22/(Eq.alpha2 + CAF22)));
-% dy(6) = RHS6;
-% dy(7) = RHS7;
-% dy(8) = (Eq.k1_hat/3)*CAF22*(1-(0.001*CAF22))*(T2/(Eq.alpha3 + T2));
-% 
-% dy(9) = T3*(1-(0.001*T3))*(Eq.k1_hat*(E2ER3/(Eq.alpha1 + E2ER3)) + Eq.k2*(CAF23/(Eq.alpha2 + CAF23)));
-% dy(10) = RHS10;
-% dy(11) = RHS11;
-% dy(12) = (Eq.k1_hat/3)*CAF23*(1-(0.001*CAF23))*(T3/(Eq.alpha3 + T3));
 end
 %%
+
